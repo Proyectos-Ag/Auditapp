6608f74cde96419ae0fa390aacd5f1a384edc03f
Jesus Soto <soleje2862004@gmail.com>
2024-08-14 14:58:28 -0600

    Correcciones

--- server/app.js
+++ server/app.js
@@ -1,35 +1,34 @@
-// app.js
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors'); 
+const cors = require('cors');
 const createError = require('http-errors');
+const dotenv = require('dotenv');
 
-const usuariosRouter = require('./routes/usuarioRoutes'); 
-const loginRoutes = require('./routes/loginRoutes'); 
+const usuariosRouter = require('./routes/usuarioRoutes');
+const loginRoutes = require('./routes/loginRoutes');
 const datosRoutes = require('./routes/datosRoutes');
 const areasRoutes = require('./routes/areasRoutes');
-const programasRoutes = require('./routes/programaRoutes')
+const programasRoutes = require('./routes/programaRoutes');
 const authRoutes = require('./routes/authRoutes');
 const fotoRoutes = require('./routes/fotoRoutes');
 const ishikawa = require('./routes/ishikawaRoutes');
 
+dotenv.config();
 
 const app = express();
 
-const dotenv = require('dotenv');
-dotenv.config();
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const mongo = require('./config/dbconfig');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors()); 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -41,14 +40,19 @@ app.use((req, res, next) => {
 });
 
 // Configura las rutas
-app.use('/usuarios', usuariosRouter); 
-app.use('/', loginRoutes); 
+app.use('/usuarios', usuariosRouter);
+app.use('/', loginRoutes);  // <- Podría estar causando conflicto con la raíz
 app.use('/datos', datosRoutes);
-app.use('/programas', programasRoutes); 
+app.use('/programas', programasRoutes);
 app.use('/areas', areasRoutes);
 app.use('/auth', authRoutes);
 app.use('/foto', fotoRoutes);
 app.use('/ishikawa', ishikawa);
+
+// Manejar la ruta raíz
+app.get('/', (req, res) => {
+  res.send('Bienvenido a la API de Aguida');
+});
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
