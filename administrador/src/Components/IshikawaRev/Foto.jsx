c65738e8e0ea325ccf27d4277fb562c6d4126655
Jesus Soto <soleje2862004@gmail.com>
2024-09-13 00:19:25 -0600

    Camara Local 6

--- administrador/src/Components/IshikawaRev/Foto.jsx
+++ administrador/src/Components/IshikawaRev/Foto.jsx
@@ -1,22 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './css/Camara.css';
 
 function Fotos({ open, onClose, onCapture }) {
   const [hayFoto, setHayFoto] = useState(false);
+  const inputRef = useRef(null);  // Referencia al input de archivo
 
   const handleCapture = (event) => {
     const file = event.target.files[0];
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
-        let base64String = reader.result;  // Aquí el prefijo ya está incluido
+        let base64String = reader.result;
 
         // Verifica si el prefijo ya está en base64String (png, jpeg, etc.)
         const prefijosBase64 = ['data:image/png;base64,', 'data:image/jpeg;base64,'];
 
         // Solo añade el prefijo si no empieza con uno de los prefijos conocidos
         if (!prefijosBase64.some(prefijo => base64String.startsWith(prefijo))) {
-          base64String = `data:image/png;base64,${base64String}`;  // Agrega el prefijo en caso de ser necesario
+          base64String = `data:image/png;base64,${base64String}`;
         }
 
         setHayFoto(true);
@@ -25,6 +26,12 @@ function Fotos({ open, onClose, onCapture }) {
       reader.readAsDataURL(file);  // Esto ya incluye 'data:image/png;base64,' o 'data:image/jpeg;base64,'
     }
   };
+
+  useEffect(() => {
+    if (open) {
+      inputRef.current.click();  // Dispara automáticamente el clic en el input de archivo
+    }
+  }, [open]);  // Se ejecuta cuando el modal se abre
 
   const cerrarFoto = () => {
     setHayFoto(false);
@@ -37,6 +44,7 @@ function Fotos({ open, onClose, onCapture }) {
       <div className="fixed-modal">
         <div className="camera-container">
           <input
+            ref={inputRef}  // Asigna la referencia al input de archivo
             type="file"
             accept="image/*"
             capture="environment" // "environment" para cámara trasera, "user" para la frontal
@@ -44,9 +52,8 @@ function Fotos({ open, onClose, onCapture }) {
             style={{ display: 'none' }} // Oculta el input
             id="cameraInput"
           />
-          <label htmlFor="cameraInput" className="camera-button">
-            <span className="material-symbols-outlined" style={{ fontSize: '40px' }}>photo_camera</span>
-          </label>
+          
+          <h1 style={{ textAlign: 'center', color: '#fff' }}>Accediendo a la cámara</h1>
 
           {hayFoto && (
             <div className="preview-container">
