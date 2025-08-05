3102081b3fd592d5a6b467a15ac428eed3bcaf9a
FredWard87 <144934868+FredWard87@users.noreply.github.com>
2025-08-05 05:40:42 -0600

    Update craco.config.js

--- auditapp/craco.config.js
+++ auditapp/craco.config.js
@@ -1,18 +1,11 @@
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      const minimizer = webpackConfig.optimization.minimizer;
-      const cssMinimizer = minimizer.find(
-        (plugin) => plugin.constructor.name === 'CssMinimizerPlugin'
+      // Eliminar el CssMinimizerPlugin completamente
+      webpackConfig.optimization.minimizer = webpackConfig.optimization.minimizer.filter(
+        (plugin) => plugin.constructor.name !== 'CssMinimizerPlugin'
       );
-
-      if (cssMinimizer) {
-        cssMinimizer.options.minimizerOptions = {
-          preset: ['default', { discardComments: { removeAll: true } }]
-        };
-      }
-
       return webpackConfig;
-    }
-  }
+    },
+  },
 };
