501c4f733d2745798a12601e544b0f0d26114ba3
FredWard87 <144934868+FredWard87@users.noreply.github.com>
2025-10-18 11:34:11 -0600

    Update loginRoutes.js

--- server/routes/loginRoutes.js
+++ server/routes/loginRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const loginController = require('../controllers/loginController');
 
 // Ruta para el inicio de sesión
-
+router.post('/login', loginController.iniciarSesion);
 
 module.exports = router;
