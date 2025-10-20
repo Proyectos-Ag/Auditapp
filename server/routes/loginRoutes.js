982560d8b9077fde134866de98df3b5c1e2d0b2d
Jesús Soto <soleje2862004@gmail.com>
2025-10-20 10:11:03 -0600

    v2.2 Implementacion de nodemon

--- server/routes/loginRoutes.js
+++ server/routes/loginRoutes.js
@@ -2,5 +2,7 @@ const express = require('express');
 const router = express.Router();
 const loginController = require('../controllers/loginController');
 
+// Ruta para el inicio de sesión
+router.post('/login', loginController.iniciarSesion);
 
 module.exports = router;
